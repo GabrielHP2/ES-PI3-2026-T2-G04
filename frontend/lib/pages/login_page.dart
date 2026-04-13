b627aa5302c627e9a9a03a0d37e2fb67196d70bc
João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
2026-04-13 16:29:15 -0300

    adds "não tenho uma conta" and "esqueci minha conta" in the login page

--- frontend/lib/pages/login_page.dart
+++ frontend/lib/pages/login_page.dart
@@ -44,15 +44,21 @@ class _LoginPageState extends State<LoginPage> {
               TextField(
                 controller: _emailController,
                 keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(labelText: 'E-mail'),
+                decoration: const InputDecoration(
+                  labelText: 'E-mail',
+                  border: OutlineInputBorder(),
+                ),
               ),
               SizedBox(height: 24),
               TextField(
                 controller: _passwordController,
                 obscureText: true,
-                decoration: const InputDecoration(labelText: 'Senha'),
+                decoration: const InputDecoration(
+                  labelText: 'Senha',
+                  border: OutlineInputBorder(),
+                ),
               ),
-              const SizedBox(height: 32),
+              SizedBox(height: 32),
               ElevatedButton(
                 onPressed: () {
                   //Log-in logic
@@ -63,8 +69,30 @@ class _LoginPageState extends State<LoginPage> {
                     borderRadius: BorderRadius.circular(16),
                   ),
                 ),
-                child: const Text('Log-in', style: TextStyle(color:Colors.white
-                ),),
+                child: const Text(
+                  'Log-in',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              SizedBox(height: 24),
+              Center(
+                child: Text(
+                  'Esqueci minha senha',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Color(0xFF5759E0), fontSize: 14),
+                ),
+              ),
+              SizedBox(height: 12),
+              Center(
+                child: Text(
+                  'Não tenho uma conta? Cadastre-se',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'inter',
+                    color: Color(0xFF1E1E1E),
+                    fontSize: 14,
+                  ),
+                ),
               ),
             ],
           ),
